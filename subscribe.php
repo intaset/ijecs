6bf3cf6674d86664e30b2100b746c7bd42202af5
Mansura Chowdhury <c.mansura@gmail.com>
2015-10-22 15:24:42 -0400

    Updated journal header

--- subscribe.php
+++ subscribe.php
@@ -129,18 +129,36 @@ if ($emailcheck2 != 0) {
     </div> <!-- Mobile -->
   </header>
 
-  <div class="j-header">
+<div class="mobile">
+<div class="j-header">
   <div class="name">
-    <h1>International Journal of Electrical and Computer Systems (IJECS)<br>
-    <p class="body">ISSN: 1929-2716</p></h1>
+    <h1>International Journal of Electrical and Computer Systems (IJECS)<br><p class="body">ISSN: 1929-2716</p></h1>
     <div class="oalink">
     <a href="/openaccess" target="blank" title="Avestia's Open Access">
-          <img src="img/j-oa.png" border="0" onmouseover="this.src='img/j-oa-hover.png'" onmouseout="this.src='img/j-oa.png'" class="j-oa">
+          <img src="../img/j-oa.png" border="0" onmouseover="this.src='../img/j-oa-hover.png'" onmouseout="this.src='../img/j-oa.png'" class="j-oa">
     </a>
   </div>
   </div>
 
-    <img src="img/journals/j-ijecs.jpg" class="j-img">
+    <img src="../img/journals/j-ijecs.jpg" class="j-img">
+</div>
+</div>
+
+<div class="desktop jhead">
+  <div class="name">
+    <h1>International Journal of Electrical and Computer Systems (IJECS)</h1>
+    <div class="issn">
+    <p class="body">ISSN: 1929-2716</p>
+    <div class="oalink">
+    <a href="/openaccess" target="blank" title="Avestia's Open Access">
+          <img src="../img/j-oa.png" border="0" onmouseover="this.src='../img/j-oa-hover.png'" onmouseout="this.src='../img/j-oa.png'" class="j-oa">
+    </a>
+   </div>
+   </div>
+  </div>
+
+  <div class="j-header">
+  </div>
 </div>
 
   <div role="navigation" class="navbar navbar-default">
@@ -462,18 +480,36 @@ mail($email, $subject, $message,  "FROM: $jName <no-reply@avestia.com>");
     </div> <!-- Mobile -->
   </header>
 
-  <div class="j-header">
+<div class="mobile">
+<div class="j-header">
   <div class="name">
-    <h1>International Journal of Electrical and Computer Systems (IJECS)<br>
-    <p class="body">ISSN: 1929-2716</p></h1>
+    <h1>International Journal of Electrical and Computer Systems (IJECS)<br><p class="body">ISSN: 1929-2716</p></h1>
     <div class="oalink">
     <a href="/openaccess" target="blank" title="Avestia's Open Access">
-          <img src="img/j-oa.png" border="0" onmouseover="this.src='img/j-oa-hover.png'" onmouseout="this.src='img/j-oa.png'" class="j-oa">
+          <img src="../img/j-oa.png" border="0" onmouseover="this.src='../img/j-oa-hover.png'" onmouseout="this.src='../img/j-oa.png'" class="j-oa">
     </a>
   </div>
   </div>
 
-    <img src="img/journals/j-ijecs.jpg" class="j-img">
+    <img src="../img/journals/j-ijecs.jpg" class="j-img">
+</div>
+</div>
+
+<div class="desktop jhead">
+  <div class="name">
+    <h1>International Journal of Electrical and Computer Systems (IJECS)</h1>
+    <div class="issn">
+    <p class="body">ISSN: 1929-2716</p>
+    <div class="oalink">
+    <a href="/openaccess" target="blank" title="Avestia's Open Access">
+          <img src="../img/j-oa.png" border="0" onmouseover="this.src='../img/j-oa-hover.png'" onmouseout="this.src='../img/j-oa.png'" class="j-oa">
+    </a>
+   </div>
+   </div>
+  </div>
+
+  <div class="j-header">
+  </div>
 </div>
 
   <div role="navigation" class="navbar navbar-default">
